8ac263683d9d16ad9f092d80f524c33706a926d1
muhammedfariis <7591faris@gmail.com>
2025-12-30 18:17:17 +0530

    day2

---
+++ leet.day2.js
@@ -0,0 +1,43 @@
+let  maximumWealth = function(accounts=[[1,2,3],[3,2,1]]) {
+    let maximumWealth = 0
+    for(let i = 0 ;i<accounts.length ; i++){
+        let sum = 0
+        for(let k = 0 ; k<accounts[i].length ; k++){
+            sum += accounts[i][k]
+        }
+        if(maximumWealth<sum){
+            maximumWealth = sum
+        }
+    }
+  
+    return maximumWealth
+};
+
+console.log(maximumWealth());
+
+
+let mostWordsFound = function(sentences=["alice and bob love leetcode", "i think so too", "this is great thanks very much"]) {
+    let maximumNumberOfWord = 0
+   for(let i = 0 ; i<sentences.length ; i++){
+    let wordCount = sentences[i].split(" ").length
+      if(wordCount > maximumNumberOfWord){
+        maximumNumberOfWord = wordCount
+      }
+      
+   }
+    return maximumNumberOfWord
+};
+
+console.log(mostWordsFound());
+
+let  kidsWithCandies = function(candies = [2,3,5,1,3], extraCandies = 3) {
+    let max = Math.max(...candies);
+    let result = [];
+    for(let i=0;i<candies.length;i++){
+        let total = candies[i] + extraCandies;
+        total >= max?result[i]= true :result[i]=false;  
+    }
+    return result;
+}
+
+console.log(kidsWithCandies());
