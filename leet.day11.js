be1797eb2f0ed66298fb8c734ecaf3caa1fa2063
muhammedfariis <7591faris@gmail.com>
2026-01-23 18:17:39 +0530

    "leetday11"

---
+++ leet.day11.js
@@ -0,0 +1,49 @@
+
+let countPrefixes = function(words =["a","b","c","ab","bc" , "a" , "ab"], s = "abc") {
+
+    let count = 0
+
+    for(let i = 0 ; i<words.length ; i++){
+       
+        let match = 0 
+
+        for(let k = 0 ; k<words[i].length ; k++){
+            if(words[i][k]===s[k]){
+                match++
+            }
+        }
+
+        if(match === words[i].length){
+            count++
+        }
+    }
+    
+    return count
+};
+
+console.log(
+    countPrefixes()
+);
+
+
+
+
+
+console.log(".................\n\n\n\n");
+
+
+
+let countPrefix = function(words =["a","b","c","ab","bc" , "a" , "ab"], s = "abc") {
+
+    let count = 0
+   for(let i = 0 ; i<words.length ; i++){
+     if(s.startsWith(words[i])){
+        count++
+     }
+   }
+    return count
+};
+
+console.log(
+    countPrefixes()
+);
