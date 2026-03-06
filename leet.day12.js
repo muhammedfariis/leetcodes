7f7cdfe2e07f08eb32f74ea6a9c5261587f29587
muhammedfariis <7591faris@gmail.com>
2026-03-06 17:40:57 +0530

    testing pushed

--- leet.day12.js
+++ leet.day12.js
@@ -1,44 +1,107 @@
-let plusOne = function (digits) {
-  for (let i = digits.length - 1; i >= 0; i--) {
-    if (digits[i] < 9) {
-      digits[i]++;
-      return digits;
-    } else {
-      digits[i] = 0;
-    }
-  }
-  digits.unshift(1);
-  return digits;
-};
+// let plusOne = function (digits) {
+//   for (let i = digits.length - 1; i >= 0; i--) {
+//     if (digits[i] < 9) {
+//       digits[i]++;
+//       return digits;
+//     } else {
+//       digits[i] = 0;
+//     }
+//   }
+//   digits.unshift(1);
+//   return digits;
+// };
 
 
 
-const oddEven = (array=[1,2,3,4,5,9,10,50,60,40,8,0,75,71,72,73,79,78,74,75,788])=>{
-  let odd = []
-  let even = []
-  for (let i = 0 ;i<array.length ; i++){
-     array[i]%2===0 ? even.push(array[i]) : odd.push(array[i])
-  }
+// const oddEven = (array=[1,2,3,4,5,9,10,50,60,40,8,0,75,71,72,73,79,78,74,75,788])=>{
+//   let odd = []
+//   let even = []
+//   for (let i = 0 ;i<array.length ; i++){
+//      array[i]%2===0 ? even.push(array[i]) : odd.push(array[i])
+//   }
  
 
-  console.log(` \t odd : ${odd} \n even : ${even}`);
+//   console.log(` \t odd : ${odd} \n even : ${even}`);
+  
+// }
+
+// oddEven()
+
+
+// const duplicate = (arr=[1,1,1,2,4,5,5,7,5,8,])=>{
+//     let dup = []
+//     let uniq = []
+//    for(let i = 0 ; i<arr.length ; i++){
+//     if(!uniq.includes(arr[i])){
+//       uniq.push(arr[i])
+//     }else{
+//      dup.push(arr[i])
+//     }
+//    }
+//    return{uniq , dup}
+// }
+
+// console.log(duplicate());
+
+
+
+
+
+const calculator = (num1 , num2 , operator)=>{
+  switch(operator){
+
+ case "*" : 
+    return num1 * num2 ;
+
+    case "+" :
+      return num1 + num2
+
+      default : 
+      return NaN
+
+}
+  
+
+}
+
+
+
+
+function call ( name,callback){
+  console.log(name + "world");
+
+  // callback()
+}
+
+function hello(){
+  console.log("hello world iam a call back");
   
 }
 
-oddEven()
+call("faris")
 
 
-const duplicate = (arr=[1,1,1,2,4,5,5,7,5,8,])=>{
-    let dup = []
-    let uniq = []
-   for(let i = 0 ; i<arr.length ; i++){
-    if(!uniq.includes(arr[i])){
-      uniq.push(arr[i])
-    }else{
-     dup.push(arr[i])
-    }
-   }
-   return{uniq , dup}
+
+function* numbes(){
+  yield {hello : "faris"};
+  yield 2 ;
+  yield 3
+
 }
 
-console.log(duplicate());
+const gen = numbes()
+
+for(let i =0 ; i<numbes.length ; i++){
+  if(yield===1){
+    console.log("hello this  is yeald");
+    gen.next()
+    
+  }else{
+   console.log(   "not running currently");
+    "not running currently"
+  }
+}
+
+
+
+
